9fe3fb5e114c110edf3a685c82a5880a0f405137
CMHyuk <alsgur9710@naver.com>
2022-06-10 12:14:55 +0900

    Feat : 비밀번호 찾기 추가

--- game/src/main/java/game/web/member/MemberController.java
+++ game/src/main/java/game/web/member/MemberController.java
@@ -2,16 +2,17 @@ package game.web.member;
 
 import game.domain.login.member.Member;
 import game.domain.login.member.MemberRepository;
+import game.domain.login.member.edit.Password;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import java.util.Optional;
 
 @Slf4j
@@ -42,6 +43,59 @@ public class MemberController {
         }
 
         memberRepository.save(member);
+        log.info("member={}", member);
         return "redirect:/";
+    }
+
+    @GetMapping("/edit")
+    public String editForm(@ModelAttribute Password password) {
+        return "members/editForm";
+    }
+
+    @PostMapping("/edit")
+    public String edit(@Validated @ModelAttribute Password password, BindingResult bindingResult, @SessionAttribute(name = "loginMember", required = false) Member loginMember,
+                        HttpServletRequest request) {
+
+        if(loginMember == null) {
+            return "home";
+        }
+
+        String nowPassword = password.getNowPassword();
+        String newPassword = password.getNewPassword();
+        String checkNewPassword = password.getCheckNewPassword();
+
+        log.info("nowPassword={}", nowPassword);
+        log.info("changePassword={}", newPassword);
+
+        if(bindingResult.hasErrors()) {
+            log.info("errors={}", bindingResult);
+            return "members/editForm";
+        }
+
+        if(!loginMember.getPassword().equals(nowPassword)) {
+            log.info("errors={}", bindingResult);
+            bindingResult.addError(new FieldError("password", "nowPassword", "현재 비밀번호와 다릅니다."));
+            return "members/editForm";
+        }
+            
+        if(nowPassword.equals(newPassword)) {
+            log.info("errors={}", bindingResult);
+            bindingResult.addError(new FieldError("password", "newPassword", "현재 비밀번호와 같습니다."));
+            return "members/editForm";
+        }
+
+        if(!newPassword.equals(checkNewPassword)) {
+            log.info("errors={}", bindingResult);
+            bindingResult.addError(new FieldError("password", "checkNewPassword", "새 비밀번호와 다릅니다."));
+            return "members/editForm";
+        }
+
+        memberRepository.edit(loginMember, newPassword);
+        log.info("editPassword={}", loginMember.getPassword());
+
+        HttpSession session = request.getSession(false);
+        session.invalidate();
+
+        return "home";
     }
 }
