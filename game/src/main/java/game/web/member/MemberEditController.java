1dd22099540b3e437645364488e134a3731f01e7
CMHyuk <alsgur9710@naver.com>
2022-06-11 22:39:53 +0900

    Rename : MemberController -> MemberEditController

--- game/src/main/java/game/web/member/MemberEditController.java
+++ game/src/main/java/game/web/member/MemberEditController.java
@@ -5,7 +5,6 @@ import game.domain.login.member.MemberRepository;
 import game.domain.login.member.edit.Password;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.validation.annotation.Validated;
@@ -13,39 +12,13 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
-import java.util.Optional;
 
 @Slf4j
-@Controller
 @RequiredArgsConstructor
 @RequestMapping("/members")
-public class MemberController {
+public class MemberEditController {
 
     private final MemberRepository memberRepository;
-
-    @GetMapping("/add")
-    public String addForm(@ModelAttribute Member member) {
-        return "members/addForm";
-    }
-
-    @PostMapping("/add")
-    public String save(@Validated @ModelAttribute Member member, BindingResult bindingResult) {
-        if(bindingResult.hasErrors()) {
-            log.info("errors={}", bindingResult);
-            return "members/addForm";
-        }
-
-        Optional<Member> loginId = memberRepository.findByLoginId(member.getLoginId());
-
-        if(!loginId.isEmpty()) {
-            bindingResult.reject("saveFail", "아이디가 존재합니다.");
-            return "members/addForm";
-        }
-
-        memberRepository.save(member);
-        log.info("member={}", member);
-        return "redirect:/";
-    }
 
     @GetMapping("/edit")
     public String editForm(@ModelAttribute Password password) {
@@ -53,8 +26,9 @@ public class MemberController {
     }
 
     @PostMapping("/edit")
-    public String edit(@Validated @ModelAttribute Password password, BindingResult bindingResult, @SessionAttribute(name = "loginMember", required = false) Member loginMember,
-                        HttpServletRequest request) {
+    public String edit(@Validated @ModelAttribute Password password, BindingResult bindingResult,
+                       @SessionAttribute(name = "loginMember", required = false) Member loginMember,
+                       HttpServletRequest request) {
 
         if(loginMember == null) {
             return "home";
@@ -77,7 +51,7 @@ public class MemberController {
             bindingResult.addError(new FieldError("password", "nowPassword", "현재 비밀번호와 다릅니다."));
             return "members/editForm";
         }
-            
+
         if(nowPassword.equals(newPassword)) {
             log.info("errors={}", bindingResult);
             bindingResult.addError(new FieldError("password", "newPassword", "현재 비밀번호와 같습니다."));
