447ee44b0092f63625cb83402a08400b8f1731ad
CMHyuk <alsgur9710@naver.com>
2022-06-13 22:18:03 +0900

    비밀번호 찾기 로직

---
+++ game/src/main/java/game/web/member/find/FindLoginPassword.java
@@ -0,0 +1,25 @@
+package game.web.member.find;
+
+import game.domain.login.member.Member;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Repository;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
+@Repository
+public class FindLoginPassword {
+
+    private static final Map<String, String> findPassword = new HashMap<>();
+
+    public Member saveFindPassword(Member member) {
+        log.info("member={}", member);
+        findPassword.put(member.getLoginId(), member.getPassword());
+        return member;
+    }
+
+    public String findByPassword(String loginId) {
+        return findPassword.get(loginId);
+    }
+}
