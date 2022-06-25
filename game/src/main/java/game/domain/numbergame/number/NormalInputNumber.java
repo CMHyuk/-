62ba8e6bf5b2dde1164314a1dde0d9b042f9d48b
CMHyuk <alsgur9710@naver.com>
2022-06-25 20:43:08 +0900

    Rename : InputNumber -> NormalInputNumber

--- game/src/main/java/game/domain/numbergame/number/NormalInputNumber.java
+++ game/src/main/java/game/domain/numbergame/number/NormalInputNumber.java
@@ -7,7 +7,7 @@ import org.hibernate.validator.constraints.Range;
 import javax.validation.constraints.NotNull;
 
 @Getter @Setter
-public class InputNumber {
+public class NormalInputNumber {
 
     @NotNull
     @Range(min = 1, max = 100)
