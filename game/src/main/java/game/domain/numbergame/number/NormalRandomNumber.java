db78367e0ed402031ae390dae920acb6ab194bc2
CMHyuk <alsgur9710@naver.com>
2022-06-25 20:41:40 +0900

    Rename : RandomNumber -> NormalRandomNumber

--- game/src/main/java/game/domain/numbergame/number/NormalRandomNumber.java
+++ game/src/main/java/game/domain/numbergame/number/NormalRandomNumber.java
@@ -5,16 +5,17 @@ import org.springframework.stereotype.Service;
 import java.util.Random;
 
 @Service
-public class RandomNumber {
+public class NormalRandomNumber implements RandomNumber {
 
     Random random = new Random();
     int rn;
 
+    @Override
     public int setNum() {
         rn = random.nextInt(100)+1; //1~100 난수 생성
         return rn;
     }
-
+    @Override
     public int getNum() {
         return rn;
     }
