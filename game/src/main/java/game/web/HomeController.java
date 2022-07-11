4fbce887258596a6cbc1251e6043f9fe6fd1c78c
CMHyuk <alsgur9710@naver.com>
2022-07-11 14:03:01 +0900

    Remove : @RequiredArgsConstructor 삭제

--- game/src/main/java/game/web/HomeController.java
+++ game/src/main/java/game/web/HomeController.java
@@ -1,20 +1,13 @@
 package game.web;
 
 import game.domain.login.member.Member;
-import game.domain.login.member.MemberRepository;
 import game.web.argumentresolver.Login;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.SessionAttribute;
 
 @Controller
-@RequiredArgsConstructor
 public class HomeController {
-
-    private final MemberRepository memberRepository;
 
     @GetMapping("/")
     public String homeLogin(@Login Member member, Model model) {
@@ -26,6 +19,4 @@ public class HomeController {
         model.addAttribute("member", member);
         return "loginHome";
     }
-
-
 }
